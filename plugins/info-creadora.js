9c97b2e2e57f6a6b69e5e41085d8d13f4b460696
Tilin <jm293229@gmail.com>
2025-06-04 02:30:16 -0600

    Update info-creadora.js

--- plugins/info-creadora.js
+++ plugins/info-creadora.js
@@ -6,12 +6,12 @@ let handler = async (m, { conn, usedPrefix, text, args, command }) => {
 try{
 let contact, number, ofc, nombre, description, correo, lugar, enlace, biog
 let pp = gataImg
-const cat = `𝙂𝙊𝙆𝙐-𝘽𝙤𝙩
+const cat = `𝗬𝗜𝗬𝗜 𝗕𝗢𝗧
 * ${bot}
 
 *---------------------*
 
-𝙂𝙊𝙆𝙐-𝘽𝙤𝙩
+ 𝗬𝗜𝗬𝗜 𝗕𝗢𝗧
 *${asistencia}*
 
 *---------------------*`
@@ -28,14 +28,14 @@ let biografia = await conn.fetchStatus(number +'@s.whatsapp.net').catch(_ => 'un
 let bio = biografia.status?.toString() || `${desc2 == '' ? lenguajeGB.smsContacto2() : desc2}`
 
 nombre = official[0][0] == String(contact[0]) ? official[0][1] : official[1][0] == String(contact[0]) ? official[1][1] : official[2][0] == String(contact[0]) ? official[2][1] : official[3][0] == String(contact[0]) ? official[3][1] : lenguajeGB.smsContacto3() 
-description = official[0][0] == String(contact[0]) ? 'Solo temas de Nekotina' : official[1][0] == String(contact[0]) ? lenguajeGB.smsContacto4() : official[2][0] == String(contact[0]) ? lenguajeGB.smsContacto4() : official[3][0] == String(contact[0]) ? lenguajeGB.smsContacto4() : desc === '' ? lenguajeGB.smsContacto5() : desc
-correo = official[0][0] == String(contact[0]) ? 'https://www.instagram.com/setting_vip1?igsh=cHJmeTM1OGs1cjZo' : official[1][0] == String(contact[0]) ? 'https://www.instagram.com/setting_vip1?igsh=cHJmeTM1OGs1cjZo' : official[2][0] == String(contact[0]) ? 'https://www.instagram.com/setting_vip1?igsh=cHJmeTM1OGs1cjZo' : mail === '' ? lenguajeGB.smsContacto6() : mail
-lugar = official[0][0] == String(contact[0]) ? 'Goku' : official[1][0] == String(contact[0]) ? 'Goku' : official[2][0] == String(contact[0]) ? 'Goku' : official[3][0] == String(contact[0]) ? 'Goku' : country === '' ? lenguajeGB.smsContacto7() : country
-enlace = official[0][0] == String(contact[0]) ? 'Goku' : official[1][0] == String(contact[0]) ? 'Goku' : official[2][0] == String(contact[0]) ? 'Goku' : official[3][0] == String(contact[0]) ? 'Goku' : md 
+description = official[0][0] == String(contact[0]) ? 'Solo temas de yiyibot' : official[1][0] == String(contact[0]) ? lenguajeGB.smsContacto4() : official[2][0] == String(contact[0]) ? lenguajeGB.smsContacto4() : official[3][0] == String(contact[0]) ? lenguajeGB.smsContacto4() : desc === '' ? lenguajeGB.smsContacto5() : desc
+correo = official[0][0] == String(contact[0]) ? 'https://www.instagram.com/tilin.ff.23' : official[1][0] == String(contact[0]) ? 'https://www.instagram.com/tilin.ff.23' : official[2][0] == String(contact[0]) ? 'https://www.instagram.com/tilin.ff.23' : mail === '' ? lenguajeGB.smsContacto6() : mail
+lugar = official[0][0] == String(contact[0]) ? 'yiyi' : official[1][0] == String(contact[0]) ? 'yiyi' : official[2][0] == String(contact[0]) ? 'yiyi' : official[3][0] == String(contact[0]) ? 'yiyi' : country === '' ? lenguajeGB.smsContacto7() : country
+enlace = official[0][0] == String(contact[0]) ? 'yiyi' : official[1][0] == String(contact[0]) ? 'yiyi' : official[2][0] == String(contact[0]) ? 'yiyi' : official[3][0] == String(contact[0]) ? 'yiyi' : md 
 
-lista.push([number, ofc, nombre, description, official[3][0] == String(contact[0]) ? null : correo, lugar, enlace, bio, official[1][0] == String(contact[0]) ? 'https://www.instagram.com/setting_vip1?igsh=cHJmeTM1OGs1cjZo' : null]) }  
-lista.push([conn.user.jid.split('@')[0], await conn.getName(conn.user.jid), packname, lenguajeGB.smsContacto8(), mail === '' ? 'https://www.instagram.com/setting_vip1?igsh=cHJmeTM1OGs1cjZo' : mail, lenguajeGB.smsContacto7(), md, bioBot, yt, ig, fb, paypal, nna])
-await conn.sendFile(m.chat, pp, 'lp.jpg', cat, fkontak, false, { contextInfo: {externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, title: gt, body: '𝙎𝙞𝙨𝙠𝙚𝙙-𝘽𝙤𝙩', previewType: 0, thumbnail: gataImg, sourceUrl: accountsgb.getRandom()}}})
+lista.push([number, ofc, nombre, description, official[3][0] == String(contact[0]) ? null : correo, lugar, enlace, bio, official[1][0] == String(contact[0]) ? 'https://www.instagram.com/tilin.ff.23' : null]) }  
+lista.push([conn.user.jid.split('@')[0], await conn.getName(conn.user.jid), packname, lenguajeGB.smsContacto8(), mail === '' ? 'https://www.instagram.com/tilin.ff.23' : mail, lenguajeGB.smsContacto7(), md, bioBot, yt, ig, fb, paypal, nna])
+await conn.sendFile(m.chat, pp, 'lp.jpg', cat, fkontak, false, { contextInfo: {externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, title: gt, body: '𝗬𝗜𝗬𝗜𝗕𝗢𝗧, previewType: 0, thumbnail: gataImg, sourceUrl: accountsgb.getRandom()}}})
 await conn.sendContactArray(m.chat, lista, null, { quoted: fkontak })
 
 } catch (e) {
